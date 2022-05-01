9c0ff4a6aaa4d0547acde3161b007473305004c6
Farit Sibgatulin <farit_sib@Farits-MacBook-Air.local>
2022-05-01 12:54:17 +0300

    Add Cart

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Header() {
+  let [cartOpen, setCartOpen] = useState(false);
+
   return (
     <header className="header">
       <div className="lmn">
@@ -8,6 +10,17 @@ export default function Header() {
           <h1>Apple World</h1>
         </div>
         <div className="menu">
+          <div className="to-cart">
+            <div
+              onClick={() => setCartOpen((cartOpen = !cartOpen))}
+              className="cart"
+            >
+              {" "}
+              Cart{" "}
+            </div>
+            {/* <img src="./images/cart.png" alt="cart icon" /> */}
+          </div>
+          {cartOpen && <div className="shop-cart"></div>}
           <ul className="nav">
             <li>iPhones</li>
             <li>iPad</li>
